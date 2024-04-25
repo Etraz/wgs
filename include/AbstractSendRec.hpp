5d0f676680f96814a238005e0be128abe7bd61e2
Etraz <mikolaj.kot.kot@student.uj.edu.pl>
2024-04-25 09:29:35 +0200

    Proposes new methods in AbstractSendRec.hpp interface.

--- include/AbstractSendRec.hpp
+++ include/AbstractSendRec.hpp
@@ -5,4 +5,9 @@
 class AbstractSendRec{
 public:
     virtual std::string getMessage(std::string) = 0;
+    // Proposition of new methods
+    // virtual std::string sendRec(std::string mess, PlayerAddress target) = 0;
+    // virtual void send(std::string mess, PlayerAddress target) = 0;
+    // PlayerAddress should be alias of some numerical type and all possible addresses shall be known
+    // before starting the game
 };
